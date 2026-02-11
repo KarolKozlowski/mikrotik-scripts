47eb22feb7b49aa56a5b0fde7d8f10098a14e3d1
Karol Kozlowski <karol.kozlowski@dotnot.pl>
2026-02-11 21:49:29 +0100

    add comment about 7777/tcp

--- tools/starrupture.sh
+++ tools/starrupture.sh
@@ -3,4 +3,8 @@
 export MIKROTIK_PUBLIC_IP=195.136.68.11
 export MIKROTIK_DEST_IP=172.16.1.65
 
-./gen-nat.py --ports '7777/udp,27015/udp' --app 'StarRupture'
+# 7777/tcp used for server control VULNERABLE
+# https://wiki.starrupture-utilities.com/en/dedicated-server/configuration
+
+# ./gen-nat.py --ports '7777/tcp,7777/udp,27015/udp' --app 'StarRupture'
+./gen-nat.py --ports '7777/tcp,7777/udp,27015/udp' --app 'StarRupture'
