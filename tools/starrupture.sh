0aaebd50460dbd99cfe3ed6c53f756523f97f66d
Karol Kozlowski <karol.kozlowski@dotnot.pl>
2026-02-11 19:22:27 +0100

    update starrupture local ip

--- tools/starrupture.sh
+++ tools/starrupture.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 export MIKROTIK_PUBLIC_IP=195.136.68.11
-export MIKROTIK_DEST_IP=172.16.1.10
+export MIKROTIK_DEST_IP=172.16.1.65
 
 ./gen-nat.py --ports '7777/udp,27015/udp' --app 'StarRupture'
